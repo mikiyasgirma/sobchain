a5b5ac6fa8af6fe9e7f3e98714bfda9e72a81b0e
mikiyasgirma <mikiyasgirma.dev@gmail.com>
2024-04-27 06:58:11 +0300

    feat: mobile landing page added

---
+++ app/components/FlashCard.tsx
@@ -0,0 +1,16 @@
+import Image from "next/image";
+
+const FlashCard = () => {
+  return (
+    <div className="bg-[#CD007A] w-[221px] h-[38px] flex items-center space-x-4">
+      <Image
+        src="/assets/mask-group.png"
+        alt="Mask Group"
+        width={38}
+        height={38}
+      />
+      <div className="text-sm">Laughably Lovable</div>
+    </div>
+  );
+};
+export default FlashCard;
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 import Image from "next/image";
 import { HiOutlineMenuAlt4 } from "react-icons/hi";
+import Marquee from "react-fast-marquee";
+import FlashCard from "./components/FlashCard";
 
 export default function Home() {
   return (
@@ -9,7 +11,7 @@ export default function Home() {
           <div className="text-[25px]">SobChain</div>
           <HiOutlineMenuAlt4 size={36} color="#ffffff" />
         </nav>
-        <div className="flex flex-col justify-center items-center py-12 space-y-6">
+        <div className="flex flex-col justify-center items-center py-10 space-y-6">
           <Image
             src="/assets/mask-group.png"
             alt="Mask Grout png"
@@ -18,6 +20,22 @@ export default function Home() {
           />
           <div className="border-text text-2xl text-center">
             Where Tears are Coins, and Your Smiles, the Reward.
+          </div>
+        </div>
+        <div className="py-10">
+          <Marquee speed={35}>
+            <FlashCard />
+          </Marquee>
+        </div>
+        <div className="py-10 space-y-6">
+          <div className="border-text text-3xl text-center">
+            Welcome to Sobchain!
+          </div>
+          <div className="text-center">
+            Where else can tears turn into treasure and smiles into rewards?
+            Only here at Sobchain! We’re not just a meme coin; we're a movement.
+            We blend humor, blockchain technology, and a sprinkle of tears to
+            create the most joyously innovative currency in the digital realm.
           </div>
         </div>
       </section>
